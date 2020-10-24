ac1be24479840db06a69b82b6532d70b31ef346c
zhaojj2209 <zhaojj2000@gmail.com>
2020-10-25 00:55:39 +0800

    Add list of months to be displayed

--- src/main/java/ay2021s1_cs2103_w16_3/finesse/model/budget/MonthlyBudget.java
+++ src/main/java/ay2021s1_cs2103_w16_3/finesse/model/budget/MonthlyBudget.java
@@ -1,6 +1,7 @@
 package ay2021s1_cs2103_w16_3.finesse.model.budget;
 
 import java.math.BigDecimal;
+import java.text.DateFormatSymbols;
 import java.time.LocalDate;
 import java.util.Arrays;
 
@@ -17,6 +18,7 @@ import javafx.collections.ObservableList;
 public class MonthlyBudget {
     private static final Amount ZERO_AMOUNT = new Amount("0");
     private static final int THIS_MONTH_INDEX = 0;
+    private static final String[] MONTHS = new DateFormatSymbols().getMonths();
 
     private ObservableAmount monthlyExpenseLimit;
     private ObservableAmount monthlySavingsGoal;
@@ -27,6 +29,7 @@ public class MonthlyBudget {
     private ObservableList<BigDecimal> monthlyExpenses;
     private ObservableList<BigDecimal> monthlyIncomes;
     private ObservableList<BigDecimal> monthlySavings;
+    private ObservableList<String> months;
 
     /**
      * Creates a {@code MonthlyBudget} with an expense limit and savings goal of $0.
@@ -41,6 +44,7 @@ public class MonthlyBudget {
         monthlyExpenses = FXCollections.observableArrayList();
         monthlyIncomes = FXCollections.observableArrayList();
         monthlySavings = FXCollections.observableArrayList();
+        months = FXCollections.observableArrayList();
     }
 
     public ObservableAmount getMonthlyExpenseLimit() {
@@ -103,25 +107,34 @@ public class MonthlyBudget {
         return FXCollections.unmodifiableObservableList(monthlySavings);
     }
 
+    public ObservableList<String> getMonths() {
+        return FXCollections.unmodifiableObservableList(months);
+    }
+
     /**
      * Calculates all information related to the user's savings.
      * These are:
      * 1. Expenses for the past number of months specified by the user
      * 2. Incomes for the past number of months specified by user
      * 3. Savings for the past number of months specified by user
+     * 4. The months to be displayed according to the number of months specified by user
      *
      * @param transactions The current list of transactions.
+     * @param numOfMonths The number of months of data the user wishes to display.
      */
     public void calculateBudgetInfo(TransactionList transactions, int numOfMonths) {
         this.monthlyExpenses.clear();
         this.monthlyIncomes.clear();
-        int thisMonth = LocalDate.now().getMonthValue();
+        this.months.clear();
+
+        int thisMonthValue = LocalDate.now().getMonthValue();
         BigDecimal[] monthlyExpenses = new BigDecimal[numOfMonths];
         BigDecimal[] monthlyIncomes = new BigDecimal[numOfMonths];
         Arrays.fill(monthlyExpenses, BigDecimal.ZERO);
         Arrays.fill(monthlyIncomes, BigDecimal.ZERO);
+
         for (Transaction transaction: transactions) {
-            int monthsBeforeToday = thisMonth - transaction.getDateValue().getMonthValue();
+            int monthsBeforeToday = thisMonthValue - transaction.getDateValue().getMonthValue();
             if (monthsBeforeToday < numOfMonths) {
                 if (transaction instanceof Expense) {
                     monthlyExpenses[monthsBeforeToday] =
@@ -132,18 +145,23 @@ public class MonthlyBudget {
                 }
             }
         }
+
         this.monthlyExpenses.addAll(Arrays.asList(monthlyExpenses));
         this.monthlyIncomes.addAll(Arrays.asList(monthlyIncomes));
+
+        for (int i = 0; i < numOfMonths; i++) {
+            months.add(MONTHS[thisMonthValue - i - 1]);
+        }
+
         calculateBudget();
         calculateSavings();
     }
 
     /**
-     * Calculates the user's remaining budget for the month.
-     * Steps:
-     * 1. Set the remaining budget to the user's monthly expense limit.
-     * 2. For each expense in the list of transactions, deduct its amount from the remaining budget.
-     * 3. The minimum remaining budget is capped at 0.
+     * Calculates the user's remaining budget for the month
+     * by deducting this month's expenses from the monthly expense limit.
+     * If the result is positive, the result is set as the remaining budget.
+     * If the result is negative, the magnitude of the result is set as the budget deficit.
      */
     public void calculateBudget() {
         BigDecimal expenseLimit = monthlyExpenseLimit.getAmount().getValue();
@@ -158,11 +176,10 @@ public class MonthlyBudget {
     }
 
     /**
-     * Calculates the user's current savings for the month.
-     * Steps:
-     * 1. Set the remaining budget to the user's monthly expense limit.
-     * 2. For each expense in the list of transactions, deduct its amount from the remaining budget.
-     * 3. The minimum remaining budget is capped at 0.
+     * Calculates the user's current savings for the month
+     * by deducting this month's expenses from this month's incomes.
+     * If the result is positive, the result is set as the current savings.
+     * If the result is negative, the magnitude of the result is set as the savings deficit.
      */
     public void calculateSavings() {
         monthlySavings.clear();
