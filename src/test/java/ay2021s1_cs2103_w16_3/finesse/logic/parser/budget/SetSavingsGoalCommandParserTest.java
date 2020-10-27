6daf008a21a6c7db53aeb2e342af0d463056531b
zhaojj2209 <zhaojj2000@gmail.com>
2020-10-27 20:56:36 +0800

    Standardise use of test constants

--- src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/parser/budget/SetSavingsGoalCommandParserTest.java
+++ src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/parser/budget/SetSavingsGoalCommandParserTest.java
@@ -2,6 +2,8 @@ package ay2021s1_cs2103_w16_3.finesse.logic.parser.budget;
 
 import static ay2021s1_cs2103_w16_3.finesse.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.AMOUNT_DESC_INTERNSHIP;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_AMOUNT_INTERNSHIP;
 import static ay2021s1_cs2103_w16_3.finesse.logic.parser.CliSyntax.PREFIX_AMOUNT;
 import static ay2021s1_cs2103_w16_3.finesse.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -19,7 +21,7 @@ public class SetSavingsGoalCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        assertParseFailure(parser, PREAMBLE_WHITESPACE, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 SetSavingsGoalCommand.MESSAGE_USAGE));
     }
 
@@ -29,7 +31,7 @@ public class SetSavingsGoalCommandParserTest {
         assertParseFailure(parser, PREFIX_AMOUNT + VALID_AMOUNT_INTERNSHIP,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetSavingsGoalCommand.MESSAGE_USAGE));
         // preamble before amount prefix
-        assertParseFailure(parser, "hello" + AMOUNT_DESC_INTERNSHIP,
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + AMOUNT_DESC_INTERNSHIP,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetSavingsGoalCommand.MESSAGE_USAGE));
     }
 
@@ -38,7 +40,7 @@ public class SetSavingsGoalCommandParserTest {
         assertParseSuccess(parser, AMOUNT_DESC_INTERNSHIP,
                 new SetSavingsGoalCommand(new Amount(VALID_AMOUNT_INTERNSHIP)));
         // whitespaces before/after keywords
-        assertParseSuccess(parser, "   " + AMOUNT_DESC_INTERNSHIP + "   ",
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + AMOUNT_DESC_INTERNSHIP + PREAMBLE_WHITESPACE,
                 new SetSavingsGoalCommand(new Amount(VALID_AMOUNT_INTERNSHIP)));
     }
 
