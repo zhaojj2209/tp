412f667969987fe55a17f965e8b6d16795512b87
Tan Wei Liang <53135010+wltan@users.noreply.github.com>
2020-11-06 20:44:38 +0800

    Exclude UI from test coverage (#287)
    * Exclude UI from test coverage
    
    * Update trailing whitespace check
    
    * Remove config/travis

--- .github/check-trailing-whitespace.sh
+++ .github/check-trailing-whitespace.sh
@@ -10,16 +10,8 @@ awk '
         ret = 0
     }
     {
-        # Only warn for markdown files (*.md) to accommodate text editors
-        # which do not properly handle trailing whitespace.
-        # (e.g. GitHub web editor)
-        if ($1 ~ /\.md$/) {
-            severity = "WARN"
-        } else {
-            severity = "ERROR"
-            ret = 1
-        }
-        print severity, $1, $2, " trailing whitespace."
+        ret = 1
+        print "ERROR", $1, $2, " trailing whitespace."
     }
     END {
         exit ret
