cf9cbb9c554dc9c4965c654c1042e2c38c4b60c6
zhaojj2209 <zhaojj2000@gmail.com>
2020-11-07 22:51:21 +0800

    Add more tests

--- src/test/java/ay2021s1_cs2103_w16_3/finesse/model/budget/MonthlyBudgetTest.java
+++ src/test/java/ay2021s1_cs2103_w16_3/finesse/model/budget/MonthlyBudgetTest.java
@@ -15,7 +15,11 @@ import ay2021s1_cs2103_w16_3.finesse.model.transaction.Amount;
 import ay2021s1_cs2103_w16_3.finesse.model.transaction.Amount.CalculatedAmount;
 
 public class MonthlyBudgetTest {
+    private static final int NUM_OF_MONTHS = 12;
+
     private final MonthlyBudget monthlyBudget = new MonthlyBudget();
+    private final Amount amountToSet = new Amount("500");
+    private final CalculatedAmount calculatedAmountToSet = new CalculatedAmount(amountToSet);
 
     @Test
     public void constructor() {
@@ -51,14 +55,59 @@ public class MonthlyBudgetTest {
     }
 
     @Test
+    public void setMonthlyExpenseLimit_validAmount_successful() {
+        MonthlyBudget monthlyBudget = new MonthlyBudget();
+        monthlyBudget.setMonthlyExpenseLimit(amountToSet);
+        assertEquals(amountToSet, monthlyBudget.getMonthlyExpenseLimit().getValue());
+    }
+
+    @Test
+    public void setMonthlySavingsGoal_validAmount_successful() {
+        MonthlyBudget monthlyBudget = new MonthlyBudget();
+        monthlyBudget.setMonthlySavingsGoal(amountToSet);
+        assertEquals(amountToSet, monthlyBudget.getMonthlySavingsGoal().getValue());
+    }
+
+    @Test
+    public void setRemainingBudget_validCalculatedAmount_successful() {
+        MonthlyBudget monthlyBudget = new MonthlyBudget();
+        monthlyBudget.setRemainingBudget(calculatedAmountToSet);
+        assertEquals(calculatedAmountToSet, monthlyBudget.getRemainingBudget().getValue());
+    }
+
+    @Test
+    public void setCurrentSavings_validCalculatedAmount_successful() {
+        MonthlyBudget monthlyBudget = new MonthlyBudget();
+        monthlyBudget.setCurrentSavings(calculatedAmountToSet);
+        assertEquals(calculatedAmountToSet, monthlyBudget.getCurrentSavings().getValue());
+    }
+
+    @Test
+    public void calculateBudgetInfo_calculatesCorrectValues_returnsFalse() {
+        MonthlyBudget monthlyBudget = new MonthlyBudget();
+
+        monthlyBudget.setMonthlyExpenseLimit(amountToSet);
+        monthlyBudget.setMonthlySavingsGoal(amountToSet);
+
+        FinanceTracker financeTracker = new FinanceTracker();
+        financeTracker.setMonthlyBudget(monthlyBudget);
+
+        financeTracker.calculateBudgetInfo();
+
+        assertEquals(calculatedAmountToSet, financeTracker.getMonthlyBudget().getRemainingBudget().getValue());
+        assertEquals(new CalculatedAmount(), financeTracker.getMonthlyBudget().getCurrentSavings().getValue());
+
+    }
+
+    @Test
     public void equals_distinctMonthlyBudgetsWithSameAttributes_returnsTrue() {
         MonthlyBudget firstMonthlyBudget = new MonthlyBudget();
         MonthlyBudget secondMonthlyBudget = new MonthlyBudget();
 
-        firstMonthlyBudget.setMonthlyExpenseLimit(new Amount("500"));
-        secondMonthlyBudget.setMonthlyExpenseLimit(new Amount("500"));
-        firstMonthlyBudget.setMonthlySavingsGoal(new Amount("500"));
-        secondMonthlyBudget.setMonthlySavingsGoal(new Amount("500"));
+        firstMonthlyBudget.setMonthlyExpenseLimit(amountToSet);
+        secondMonthlyBudget.setMonthlyExpenseLimit(amountToSet);
+        firstMonthlyBudget.setMonthlySavingsGoal(amountToSet);
+        secondMonthlyBudget.setMonthlySavingsGoal(amountToSet);
 
         assertNotSame(firstMonthlyBudget, secondMonthlyBudget);
         assertEquals(firstMonthlyBudget, secondMonthlyBudget);
@@ -68,17 +117,19 @@ public class MonthlyBudgetTest {
     public void equals_sameSetValuesDifferentCalculatedValues_returnsFalse() {
         MonthlyBudget monthlyBudget = new MonthlyBudget();
 
-        monthlyBudget.setMonthlyExpenseLimit(new Amount("500"));
-        monthlyBudget.setMonthlySavingsGoal(new Amount("500"));
+        monthlyBudget.setMonthlyExpenseLimit(amountToSet);
+        monthlyBudget.setMonthlySavingsGoal(amountToSet);
 
         FinanceTracker financeTracker = new FinanceTracker();
         financeTracker.setTransactions(getTypicalTransactions());
         financeTracker.setMonthlyBudget(monthlyBudget);
 
+        // ensure monthly budgets were equal before calculating budget info
         assertEquals(financeTracker.getMonthlyBudget(), monthlyBudget);
 
         financeTracker.calculateBudgetInfo();
 
+        // monthly budgets are now not equal
         assertNotEquals(financeTracker.getMonthlyBudget(), monthlyBudget);
     }
 
